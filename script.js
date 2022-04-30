fa9599fff103125808a4f6aa47f6b7a3f7af7119
Grant Dixon <102075078+grantdix49@users.noreply.github.com>
2022-04-29 19:52:42 -0500

    Final Commit

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+const gallery = document.getElementById('imageGallery');
 
+const images = ["./images/IMG_1374.JPG", "./images/IMG_1790.JPG", "./images/IMG_4129.JPG", "./images/IMG_4689.JPG", "./images/IMG_5003.JPG"]
+
+const nextButton = document.getElementById('next');
+
+const previousButton = document.getElementById('previous');
+
+let currentImage = 0;
+
+gallery.style.backgroundImage = "url("+images[currentImage]+")";
+
+const imageUp = () => {
+    if (currentImage >= 0 && currentImage < 4) {
+        currentImage++;
+    } else {
+        currentImage = 0;
+    }
+}
+
+const imageDown = () => {
+    if (currentImage > 0 && currentImage <= 4) {
+        currentImage--;
+    } else {
+        currentImage = 4;
+    }
+}
+
+const toggleImageUp = () => {
+    imageUp();
+    gallery.style.backgroundImage = "url("+images[currentImage]+")";
+}
+
+const toggleImageDown = () => {
+    imageDown();
+    gallery.style.backgroundImage = "url("+images[currentImage]+")";
+}
+
+nextButton.addEventListener('click', toggleImageUp);
+previousButton.onclick = toggleImageDown;
